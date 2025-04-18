dc45ba7213a4ada7c7d875a2104a40d309e2eee2
Майков Семен Андреевич <Maikov.SA@tpprf.loc>
2025-04-18 12:15:55 +0300

    бновлён

--- app.py
+++ app.py
@@ -210,5 +210,5 @@ if __name__ == '__main__':
         db.create_all()
         create_admin_user()
     Thread(target=run_bot).start()
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
 
